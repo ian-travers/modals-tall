26c36d15fe688b832aac60787ec6943212c53e73
Ian Travers <mr.itpe@gmail.com>
2021-03-29 11:50:17 +0300

    Wrapper to blur content under the modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,13 +15,20 @@
 
             <main class="col-span-9">
                 <p class="mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur cupiditate deserunt esse et eveniet fuga inventore iusto perspiciatis ratione, vel veritatis voluptas! Laborum neque numquam sint sit vero voluptatem.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur cupiditate deserunt esse
+                    et eveniet fuga inventore iusto perspiciatis ratione, vel veritatis voluptas! Laborum neque numquam
+                    sint sit vero voluptatem.
                 </p>
                 <p class="mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum non, numquam. Accusantium atque aut autem, delectus doloremque est et excepturi inventore labore modi nihil, nisi obcaecati officiis possimus reiciendis rem repellat tempora voluptate?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum non, numquam. Accusantium atque aut
+                    autem, delectus doloremque est et excepturi inventore labore modi nihil, nisi obcaecati officiis
+                    possimus reiciendis rem repellat tempora voluptate?
                 </p>
                 <p class="mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cumque delectus hic itaque nulla quaerat reprehenderit saepe voluptate. Aliquid beatae doloremque inventore iusto laboriosam omnis praesentium reiciendis! Ab consectetur deleniti dignissimos eius exercitationem explicabo illo iure magni necessitatibus odit perspiciatis quas sapiente, similique vel.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cumque delectus hic itaque nulla
+                    quaerat reprehenderit saepe voluptate. Aliquid beatae doloremque inventore iusto laboriosam omnis
+                    praesentium reiciendis! Ab consectetur deleniti dignissimos eius exercitationem explicabo illo iure
+                    magni necessitatibus odit perspiciatis quas sapiente, similique vel.
                 </p>
             </main>
         </div>
@@ -33,7 +40,28 @@
     </div>
 
     {{-- modal --}}
-    <div class="bg-white p-4 shadow-md max-w-md h-44 m-auto rounded-md fixed inset-0">
-        I am a modal
+    <div>
+        <div class="fixed inset-0 bg-gray-900 opacity-60"></div>
+
+        <div class="bg-gray-50 p-4 shadow-md max-w-md h-44 m-auto rounded-md fixed inset-0">
+            <div class="flex flex-col h-full justify-between">
+                <header>
+                    <h3 class="font-semibold text-lg">Are you sure?</h3>
+                </header>
+                <main class="my-4">
+                    If you proceed, your account will be deleted entirely
+                </main>
+                <footer class="space-x-1">
+                    <button
+                        class="px-4 py-2 text-sm bg-gray-400 hover:bg-gray-500 text-gray-50 transition-all duration-200 rounded-md">
+                        Cancel
+                    </button>
+                    <button
+                        class="px-4 py-2 text-sm bg-blue-400 hover:bg-blue-500 text-gray-50 transition-all duration-200 rounded-md">
+                        Continue
+                    </button>
+                </footer>
+            </div>
+        </div>
     </div>
 </x-layout-app>
