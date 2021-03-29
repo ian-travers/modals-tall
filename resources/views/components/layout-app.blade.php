dc9711a0c33dda6b45d99bb00a791ad4cccbc90b
Ian Travers <mr.itpe@gmail.com>
2021-03-29 15:29:20 +0300

    Update app layout

--- resources/views/components/layout-app.blade.php
+++ resources/views/components/layout-app.blade.php
@@ -10,7 +10,30 @@
 </head>
 <body class="antialiased bg-gray-200">
 
-{{ $slot }}
+<div class="max-w-xl bg-green-100 mx-auto">
+    <header class="bg-blue-500 p-4">
+        <h1 class="font-bold text-gray-50 text-xl">My Site</h1>
+    </header>
+
+    <div class="grid grid-cols-12 p-4">
+        <aside class="mr-6 col-span-3">
+            <ul>
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+            </ul>
+        </aside>
+
+        <main class="col-span-9">
+            {{ $slot }}
+        </main>
+    </div>
+
+    <footer class="p-4 flex items-center justify-between bg-blue-800 text-gray-50">
+        <h5>My Site</h5>
+        <p>2021</p>
+    </footer>
+</div>
 
 @livewireScripts
 <script src="{{ mix('js/app.js', 'build') }}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,47 +1,24 @@
 <x-layout-app>
-    <div class="max-w-xl bg-green-100 mx-auto">
-        <header class="bg-blue-500 p-4">
-            <h1 class="font-bold text-gray-50 text-xl">My Site</h1>
-        </header>
+    <p class="mb-4">
+        Would you like to delete your account?
+    </p>
 
-        <div class="grid grid-cols-12 p-4">
-            <aside class="mr-6 col-span-3">
-                <ul>
-                    <li>Link 1</li>
-                    <li>Link 2</li>
-                    <li>Link 3</li>
-                </ul>
-            </aside>
-
-            <main class="col-span-9">
-                <p class="mb-4">
-                    Would you like to delete your account?
-                </p>
-
-                <form
-                    id="delete-user-form"
-                    action="/"
-                    method="post"
-                    x-data
-                    @submit.prevent="
+    <form
+        id="delete-user-form"
+        action="/"
+        method="post"
+        x-data
+        @submit.prevent="
                         location.hash='#user-delete-modal'
                     "
-                >
-                    @csrf
-                    <p class="mb-4">
-                        <x-button class="bg-blue-400 hover:bg-blue-500 text-gray-50">
-                            Yes, Delete
-                        </x-button>
-                    </p>
-                </form>
-            </main>
-        </div>
-
-        <footer class="p-4 flex items-center justify-between bg-blue-800 text-gray-50">
-            <h5>My Site</h5>
-            <p>2021</p>
-        </footer>
-    </div>
+    >
+        @csrf
+        <p class="mb-4">
+            <x-button class="bg-blue-400 hover:bg-blue-500 text-gray-50">
+                Yes, Delete
+            </x-button>
+        </p>
+    </form>
 
     {{-- modal --}}
     <x-confirmation-modal hash="#user-delete-modal">
