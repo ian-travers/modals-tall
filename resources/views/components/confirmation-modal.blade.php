220e960cb339a617cad4b4b6c2896efea20192e7
Ian Travers <mr.itpe@gmail.com>
2021-03-29 14:02:32 +0300

    Extract confirmation-modal blade

---
+++ resources/views/components/confirmation-modal.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <div class="fixed inset-0 bg-gray-900 opacity-60"></div>
+
+    <div class="bg-gray-50 p-4 shadow-md max-w-md h-44 m-auto rounded-md fixed inset-0">
+        <div class="flex flex-col h-full justify-between">
+            <header>
+                <h3 class="font-semibold text-lg">Are you sure?</h3>
+            </header>
+            <main class="my-4">
+                If you proceed, your account will be deleted entirely
+            </main>
+            <footer class="space-x-1">
+                <button
+                    class="px-4 py-2 text-sm bg-gray-400 hover:bg-gray-500 text-gray-50 transition-all duration-200 rounded-md">
+                    Cancel
+                </button>
+                <button
+                    class="px-4 py-2 text-sm bg-blue-400 hover:bg-blue-500 text-gray-50 transition-all duration-200 rounded-md">
+                    Continue
+                </button>
+            </footer>
+        </div>
+    </div>
+</div>
