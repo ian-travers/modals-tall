444bab636858d17772b757d898e146ebc4c35501
Ian Travers <mr.itpe@gmail.com>
2021-03-30 10:17:45 +0300

    Update modal footer && x-cloak (alpinejs)

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -3,20 +3,21 @@
     x-data="{ show: @entangle($attributes->wire('model')) }"
     x-show="show"
     @keydown.escape.window="show = false"
+    x-cloak
 >
     <div @click="show = false" class="fixed inset-0 bg-gray-900 opacity-60"></div>
 
-    <div class="bg-gray-50 p-4 shadow-md max-w-md h-44 m-auto rounded-md fixed inset-0">
+    <div class="bg-gray-50 shadow-md max-w-md h-44 m-auto rounded-md fixed inset-0">
         <div class="flex flex-col h-full justify-between">
-            <header>
+            <header class="p-6">
                 <h3 class="font-semibold text-lg">
                     {{ $title }}
                 </h3>
             </header>
-            <main class="my-4">
+            <main class="mb-4 px-6">
                 {{ $slot }}
             </main>
-            <footer class="space-x-1">
+            <footer class="flex justify-end px-6 py-4 space-x-2 bg-gray-200 rounded-b-md">
                 {{ $footer }}
             </footer>
         </div>
