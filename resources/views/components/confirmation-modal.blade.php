dab16e7d34fcc06c4d53defc20fa0827dcab0e22
Ian Travers <mr.itpe@gmail.com>
2021-03-30 10:20:48 +0300

    Add basic transition (alpinejs)

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -7,7 +7,7 @@
 >
     <div @click="show = false" class="fixed inset-0 bg-gray-900 opacity-60"></div>
 
-    <div class="bg-gray-50 shadow-md max-w-md h-44 m-auto rounded-md fixed inset-0">
+    <div class="bg-gray-50 shadow-md max-w-md h-44 m-auto rounded-md fixed inset-0" x-show.transition="show">
         <div class="flex flex-col h-full justify-between">
             <header class="p-6">
                 <h3 class="font-semibold text-lg">
